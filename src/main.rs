6242b8bca60ebf8a6edcb92edcd8b37f93992299
Alex Chi <iskyzh@gmail.com>
2020-07-27 17:08:54 +0800

    update font loading scheme

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,6 @@ use threadpool::ThreadPool;
 pub use vec3::Vec3;
 
 const AUTHOR: &str = "Alex Chi";
-// 1. select your favorite font on Google Fonts.
-// 2. download it into output directory.
-// 3. change the following line.
-// 4. change L23 and L48 in .github/workflows/cargo.yml
-const FONT_DATA: &[u8] = include_bytes!("../output/EncodeSans-Regular.ttf");
 
 struct World {
     pub height: u32,
@@ -30,13 +25,43 @@ fn get_text() -> String {
     // only available on GitHub Action
     let github_sha = option_env!("GITHUB_SHA")
         .map(|x| "@".to_owned() + &x[0..6])
-        .unwrap_or("".to_owned());
+        .unwrap_or(|| "".to_owned());
     format!("{}{}", AUTHOR, github_sha)
+}
+
+fn is_ci() -> bool {
+    option_env!("CI").unwrap_or("") == "true"
+}
+
+fn render_text(image: &mut RgbImage, msg: &str) {
+    let font_file = if is_ci() {
+        "EncodeSans-Regular.ttf"
+    } else {
+        "/System/Library/Fonts/Helvetica.ttc"
+    };
+    let font_path = std::env::current_dir().unwrap().join(font_file);
+    let data = std::fs::read(&font_path).unwrap();
+    let font: Font = Font::try_from_vec(data).unwrap_or_else(|| {
+        panic!(format!(
+            "error constructing a Font from data at {:?}",
+            font_path
+        ));
+    });
+
+    imageproc::drawing::draw_text_mut(
+        image,
+        Rgb([255, 255, 255]),
+        10,
+        10,
+        rusttype::Scale::uniform(24.0),
+        &font,
+        msg,
+    );
 }
 
 fn main() {
     // get environment variable CI, which is true for GitHub Action
-    let is_ci = option_env!("CI").unwrap_or("") == "true";
+    let is_ci = is_ci();
 
     // jobs: split image into how many parts
     // workers: maximum allowed concurrent running threads
@@ -100,21 +125,10 @@ fn main() {
     bar.finish();
 
     // render commit ID and author name on image
-    let render_text = get_text();
+    let msg = get_text();
+    println!("Extra Info: {}", msg);
 
-    println!("Extra Info: {}", render_text);
-
-    let font: Font<'static> = Font::try_from_bytes(FONT_DATA).unwrap();
-
-    imageproc::drawing::draw_text_mut(
-        &mut result,
-        Rgb([255, 255, 255]),
-        10,
-        10,
-        rusttype::Scale::uniform(24.0),
-        &font,
-        render_text.as_str(),
-    );
+    render_text(&mut result, msg.as_str());
 
     result.save("output/test.png").unwrap();
 }
