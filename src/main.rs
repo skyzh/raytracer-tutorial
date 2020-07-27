79849f0438520c823fa1f1f12c2c2a74c566024b
Alex Chi <iskyzh@gmail.com>
2020-07-27 17:20:04 +0800

    ci: render extra info on image (#8)
    * ci: render text on image
    
    * fix clippy
    
    * update font loading scheme
    
    * fix clippy warnings
    
    * trigger CI
    
    * fix clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #[allow(clippy::float_cmp)]
 mod vec3;
-use image::{ImageBuffer, RgbImage};
+use image::{ImageBuffer, Rgb, RgbImage};
 use indicatif::ProgressBar;
+use rusttype::Font;
 use std::sync::mpsc::channel;
 use std::sync::Arc;
 use threadpool::ThreadPool;
-
 pub use vec3::Vec3;
+
+const AUTHOR: &str = "Alex Chi";
 
 struct World {
     pub height: u32,
@@ -18,12 +20,51 @@ impl World {
     }
 }
 
-fn main() {
-    let is_ci = match std::env::var("CI") {
-        Ok(x) => x == "true",
-        Err(_) => false,
-    };
+fn get_text() -> String {
+    // GITHUB_SHA is the associated commit ID
+    // only available on GitHub Action
+    let github_sha = option_env!("GITHUB_SHA")
+        .map(|x| "@".to_owned() + &x[0..6])
+        .unwrap_or_default();
+    format!("{}{}", AUTHOR, github_sha)
+}
 
+fn is_ci() -> bool {
+    option_env!("CI").unwrap_or_default() == "true"
+}
+
+fn render_text(image: &mut RgbImage, msg: &str) {
+    let font_file = if is_ci() {
+        "EncodeSans-Regular.ttf"
+    } else {
+        "/System/Library/Fonts/Helvetica.ttc"
+    };
+    let font_path = std::env::current_dir().unwrap().join(font_file);
+    let data = std::fs::read(&font_path).unwrap();
+    let font: Font = Font::try_from_vec(data).unwrap_or_else(|| {
+        panic!(format!(
+            "error constructing a Font from data at {:?}",
+            font_path
+        ));
+    });
+
+    imageproc::drawing::draw_text_mut(
+        image,
+        Rgb([255, 255, 255]),
+        10,
+        10,
+        rusttype::Scale::uniform(24.0),
+        &font,
+        msg,
+    );
+}
+
+fn main() {
+    // get environment variable CI, which is true for GitHub Action
+    let is_ci = is_ci();
+
+    // jobs: split image into how many parts
+    // workers: maximum allowed concurrent running threads
     let (n_jobs, n_workers): (usize, usize) = if is_ci { (32, 2) } else { (16, 2) };
 
     println!(
@@ -34,29 +75,35 @@ fn main() {
     let height = 512;
     let width = 1024;
 
+    // create a channel to send objects between threads
     let (tx, rx) = channel();
     let pool = ThreadPool::new(n_workers);
 
     let bar = ProgressBar::new(n_jobs as u64);
 
+    // use Arc to pass one instance of World to multiple threads
     let world = Arc::new(World { height });
 
     for i in 0..n_jobs {
         let tx = tx.clone();
         let world_ptr = world.clone();
         pool.execute(move || {
+            // here, we render some of the rows of image in one thread
             let row_begin = height as usize * i / n_jobs;
             let row_end = height as usize * (i + 1) / n_jobs;
             let render_height = row_end - row_begin;
             let mut img: RgbImage = ImageBuffer::new(width, render_height as u32);
             for x in 0..width {
+                // img_y is the row in partial rendered image
+                // y is real position in final image
                 for (img_y, y) in (row_begin..row_end).enumerate() {
                     let y = y as u32;
                     let pixel = img.get_pixel_mut(x, img_y as u32);
                     let color = world_ptr.color(x, y);
-                    *pixel = image::Rgb([color, color, color]);
+                    *pixel = Rgb([color, color, color]);
                 }
             }
+            // send row range and rendered image to main thread
             tx.send((row_begin..row_end, img))
                 .expect("failed to send result");
         });
@@ -65,6 +112,7 @@ fn main() {
     let mut result: RgbImage = ImageBuffer::new(width, height);
 
     for (rows, data) in rx.iter().take(n_jobs) {
+        // idx is the corrsponding row in partial-rendered image
         for (idx, row) in rows.enumerate() {
             for col in 0..width {
                 let row = row as u32;
@@ -74,7 +122,13 @@ fn main() {
         }
         bar.inc(1);
     }
+    bar.finish();
+
+    // render commit ID and author name on image
+    let msg = get_text();
+    println!("Extra Info: {}", msg);
+
+    render_text(&mut result, msg.as_str());
 
     result.save("output/test.png").unwrap();
-    bar.finish();
 }
